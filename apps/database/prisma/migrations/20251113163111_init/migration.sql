17278b1167573f65d5976e23dda8d023ea2deff9
shivasaklani1@gmail.com <shivasaklani1@gmail.com>
2025-11-13 23:02:19 +0530

    changes_not_finished

---
+++ apps/database/prisma/migrations/20251113163111_init/migration.sql
@@ -0,0 +1,99 @@
+-- CreateEnum
+CREATE TYPE "webstatus" AS ENUM ('up', 'down', 'degraded');
+
+-- CreateEnum
+CREATE TYPE "eventlevel" AS ENUM ('low', 'mid', 'high', 'threat');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "profileImage" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Website" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "user_id" TEXT,
+    "timeAdded" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Website_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WebEvents" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "level" "eventlevel" NOT NULL,
+    "resolved" BOOLEAN DEFAULT false,
+    "website_id" TEXT NOT NULL,
+    "resolvedTime" TIMESTAMP(3) NOT NULL,
+    "timeAdded" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WebEvents_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Region" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "user_id" TEXT,
+
+    CONSTRAINT "Region_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WebsiteTick" (
+    "id" TEXT NOT NULL,
+    "response_time_ms" INTEGER NOT NULL,
+    "status" "webstatus" NOT NULL,
+    "region_id" TEXT NOT NULL,
+    "website_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WebsiteTick_pkey" PRIMARY KEY ("id","createdAt")
+);
+
+-- CreateTable
+CREATE TABLE "Channels" (
+    "id" TEXT NOT NULL,
+    "channelName" TEXT NOT NULL,
+    "channelDetails" TEXT NOT NULL,
+    "level" "eventlevel" NOT NULL,
+    "website_id" TEXT NOT NULL,
+    "monitor" BOOLEAN NOT NULL DEFAULT true,
+    "lastSent" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Channels_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "WebsiteTick_createdAt_idx" ON "WebsiteTick"("createdAt");
+
+-- AddForeignKey
+ALTER TABLE "Website" ADD CONSTRAINT "Website_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WebEvents" ADD CONSTRAINT "WebEvents_website_id_fkey" FOREIGN KEY ("website_id") REFERENCES "Website"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Region" ADD CONSTRAINT "Region_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WebsiteTick" ADD CONSTRAINT "WebsiteTick_region_id_fkey" FOREIGN KEY ("region_id") REFERENCES "Region"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WebsiteTick" ADD CONSTRAINT "WebsiteTick_website_id_fkey" FOREIGN KEY ("website_id") REFERENCES "Website"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Channels" ADD CONSTRAINT "Channels_website_id_fkey" FOREIGN KEY ("website_id") REFERENCES "Website"("id") ON DELETE CASCADE ON UPDATE CASCADE;
